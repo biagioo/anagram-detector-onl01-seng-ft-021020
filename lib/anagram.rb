595a174b7e72bfd566afee4616169cdf89ad37b3
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-22 20:45:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,12 +4,12 @@ class Anagram
   attr_accessor :word
   
   def initialize(word) 
-    @word = word 
+    @word = word.sort 
   end 
   
   def match(words) 
     words.collect do |word| 
-      if @word.sort == word.sort
+      if @word == word.sort
         word 
       end 
     end 
