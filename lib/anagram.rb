1d8521636abe1fa1357ac2365db0e7407a6930fd
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-22 21:05:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,15 +5,15 @@ class Anagram
   
   def initialize(word) 
     word_split = word.split("")
-  binding.pry
+    @word = word_split.sort
   end 
   
   def match(words) 
     words.collect do |word| 
      word_split = word.split("")
-      
-      
-      
+      if @word == word_split 
+        word 
+      end 
      # binding.pry
     end 
   end 
